75e68ebfba1e6e2450d57e45882c3c802a34763b
Alumno <urquiza.gamig24@triana.salesianos.edu>
2026-03-04 14:23:45 +0100

    <fix>[Fotos players]

--- database/seeders/PlayersSeeder.php
+++ database/seeders/PlayersSeeder.php
@@ -15,22 +15,22 @@ class PlayersSeeder extends Seeder
         $now = Carbon::now();
 
         $players = [
-            ['name' => 'BrokenBlade', 'team' => 'G2 Esports', 'pos' => 'Top', 'country' => 'Alemania', 'kda' => 3.5],
-            ['name' => 'Caps', 'team' => 'G2 Esports', 'pos' => 'Mid', 'country' => 'Europa', 'kda' => 4.2],
-            ['name' => 'Mikyx', 'team' => 'G2 Esports', 'pos' => 'Support', 'country' => 'Europa', 'kda' => 3.1],
-            ['name' => 'Faker', 'team' => 'T1', 'pos' => 'Mid', 'country' => 'Corea del Sur', 'kda' => 5.0],
-            ['name' => 'Gumayusi', 'team' => 'T1', 'pos' => 'Adc', 'country' => 'Corea del Sur', 'kda' => 4.8],
-            ['name' => 'Elyoya', 'team' => 'MAD Lions KOI', 'pos' => 'Jungle', 'country' => 'España', 'kda' => 3.9],
-            ['name' => 'Supator', 'team' => 'MAD Lions KOI', 'pos' => 'Mid', 'country' => 'España', 'kda' => 3.2],
-            ['name' => 'Razork', 'team' => 'Fnatic', 'pos' => 'Jungle', 'country' => 'España', 'kda' => 4.1],
-            ['name' => 'Humanoid', 'team' => 'Fnatic', 'pos' => 'Mid', 'country' => 'Europa', 'kda' => 3.7],
-            ['name' => 'Hans Sama', 'team' => 'G2 Esports', 'pos' => 'Adc', 'country' => 'Francia', 'kda' => 4.5],
+            ['name' => 'BrokenBlade', 'photo' => 'https://ak-deeplol-ddragon-cdn.deeplol.gg/pro/BrokenBlade.png', 'team' => 'G2 Esports', 'pos' => 'Top', 'country' => 'Alemania', 'kda' => 3.5],
+            ['name' => 'Caps', 'photo' => 'https://ak-deeplol-ddragon-cdn.deeplol.gg/pro/Caps.png', 'team' => 'G2 Esports', 'pos' => 'Mid', 'country' => 'Europa', 'kda' => 4.2],
+            ['name' => 'Mikyx', 'photo' => 'https://ak-deeplol-ddragon-cdn.deeplol.gg/pro/Mikyx.png', 'team' => 'G2 Esports', 'pos' => 'Support', 'country' => 'Europa', 'kda' => 3.1],
+            ['name' => 'Faker', 'photo' => 'https://ak-deeplol-ddragon-cdn.deeplol.gg/pro/Faker.png', 'team' => 'T1', 'pos' => 'Mid', 'country' => 'Corea del Sur', 'kda' => 5.0],
+            ['name' => 'Gumayusi', 'photo' => 'https://ak-deeplol-ddragon-cdn.deeplol.gg/pro/Gumayusi.png', 'team' => 'T1', 'pos' => 'Adc', 'country' => 'Corea del Sur', 'kda' => 4.8],
+            ['name' => 'Elyoya', 'photo' => 'https://ak-deeplol-ddragon-cdn.deeplol.gg/pro/Elyoya.png', 'team' => 'MAD Lions KOI', 'pos' => 'Jungle', 'country' => 'España', 'kda' => 3.9],
+            ['name' => 'Supator', 'photo' => '', 'team' => 'MAD Lions KOI', 'pos' => 'Mid', 'country' => 'España', 'kda' => 3.2],
+            ['name' => 'Razork', 'photo' => '', 'team' => 'Fnatic', 'pos' => 'Jungle', 'country' => 'España', 'kda' => 4.1],
+            ['name' => 'Humanoid', 'photo' => '', 'team' => 'Fnatic', 'pos' => 'Mid', 'country' => 'Europa', 'kda' => 3.7],
+            ['name' => 'Hans Sama', 'photo' => '', 'team' => 'G2 Esports', 'pos' => 'Adc', 'country' => 'Francia', 'kda' => 4.5],
         ];
 
         foreach ($players as $p) {
             DB::table('players')->insert([
                 'name' => $p['name'],
-                'photo' => '',
+                'photo' => $p['photo'],
                 'team_id' => $teams[$p['team']],
                 'kda' => $p['kda'],
                 'position' => $p['pos'],
